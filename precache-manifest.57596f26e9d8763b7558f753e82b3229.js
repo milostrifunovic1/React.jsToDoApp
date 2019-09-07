d050a905bc0fb48d8f1e1a77715831bc3dd69fb0
milostrifunovic1 <milos.trifunovic.92@gmail.com>
2019-09-07 16:02:06 +0200

    Updates

---
+++ precache-manifest.57596f26e9d8763b7558f753e82b3229.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa65dcead8a8a6737629aa3effe0709e",
+    "url": "/React.jsToDoApp/index.html"
+  },
+  {
+    "revision": "681ee165979119eb681c",
+    "url": "/React.jsToDoApp/static/css/main.07f5c958.chunk.css"
+  },
+  {
+    "revision": "b8dd2ea071e7b5088ffa",
+    "url": "/React.jsToDoApp/static/js/2.86e1eef8.chunk.js"
+  },
+  {
+    "revision": "681ee165979119eb681c",
+    "url": "/React.jsToDoApp/static/js/main.f8c4e1fe.chunk.js"
+  },
+  {
+    "revision": "f1f9bbaf543e1bbf1d88",
+    "url": "/React.jsToDoApp/static/js/runtime~main.030658c5.js"
+  }
+]);
